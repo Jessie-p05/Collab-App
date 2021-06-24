7547d1ed8210c8a823f851776ada09ff64c7d217
Jessie-p05 <jxpei05@gmail.com>
2021-06-24 16:22:37 -0600

    move sequlize-cli to dependencies

--- config/config.js
+++ config/config.js
@@ -19,6 +19,8 @@ module.exports = {
     "password": process.env.PGPASSWORD,
     "database": process.env.PGDATABASE,
     "host": process.env.PGHOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "operatorsAliases": false,
+    "use_env_variable": "DATABASE_URL"
   }
 }
